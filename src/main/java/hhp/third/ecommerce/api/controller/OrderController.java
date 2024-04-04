5e971fc81abd7d9892760568b8db0f6998d91344
DYKIM\kdy95 <kdy950930@gmail.com>
2024-04-04 21:49:42 +0900

    Controller 생성

---
+++ src/main/java/hhp/third/ecommerce/api/controller/OrderController.java
@@ -0,0 +1,15 @@
+package hhp.third.ecommerce.api.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @PostMapping("{userId}")
+    public void add(@PathVariable("userId") String userId){
+
+    }
+}
