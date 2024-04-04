f5c71629c9dcd29eba3d69de4fd003751f300460
DYKIM\kdy95 <kdy950930@gmail.com>
2024-04-04 19:46:26 +0900

    first commit

---
+++ src/test/java/hhp/third/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package hhp.third.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
